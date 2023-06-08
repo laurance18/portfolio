90b903cd9c3728fdac4ba9c3e6057b1244e84f92
Deniz Işık <denizisik2004@gmail.com>
2023-06-08 14:16:19 +0300

    First template of Navbar

--- portfolio-site/src/components/Navbar.js
+++ portfolio-site/src/components/Navbar.js
@@ -1,7 +1,12 @@
 export default function Navbar() {
     return (
-        <nav>
-            
+        <nav className="bg-secondary flex justify-between xl:justify-between items-center py-4 px-6">
+            <h2 className="text-white xl:text-xl text-sm">Deniz Işık</h2>
+            <div className="nav--links">
+                <a href="#about" className="text-white xl:text-base text-[10px] hover:text-gray-300 px-2 xl:px-4">Skills</a>
+                <a href="#about" className="text-white xl:text-base text-[10px] hover:text-gray-300 px-2 xl:px-4">Projects</a>
+                <a href="#about" className="text-white xl:text-base text-[10px] hover:text-gray-300 px-2 xl:px-4">Contact</a>
+            </div>
         </nav>
     )
 }
