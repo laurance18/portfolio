1a05b77d4f592d76b8a77cb846ea9b6074495cb5
Deniz Işık <denizisik2004@gmail.com>
2023-06-08 19:53:52 +0300

    Update Footer.js

--- portfolio-site/src/components/Footer.js
+++ portfolio-site/src/components/Footer.js
@@ -11,9 +11,9 @@ export default function Footer() {
                 </footer>
             </BrowserView>
             <MobileView>
-                <footer className="bg-gradient-to-b from-primary to-secondary flex justify-center items-center gap-3 py-2 static bottom-0 w-full">
+                <footer className="bg-gradient-to-b from-primary to-secondary flex justify-center items-center gap-3 pt-2 pb-3 static bottom-0 w-full">
                     <a href="https://github.com/laurance18" className="duration-700 hover:rotate-[360deg] hover:scale-110"><img src={githubLogo} alt="githubLogo" /></a> 
-                    <p className="text-gray-200 text-sm">Made by Laurance18</p>
+                    <p className="text-gray-200 text-[10px]">Made by Laurance18</p>
                 </footer>
             </MobileView>
         </>
