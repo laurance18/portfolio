135f4c62a38cb8bdf546b8d2398cd7ddff7bfc33
Deniz Işık <denizisik2004@gmail.com>
2023-06-08 18:15:04 +0300

    Add footer

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App bg-primary h-screen w-screen bg-contain">
+    <div className="App bg-primary h-screen bg-contain">
       {/* Code below is published on branch gh-pages. */}
       {/* <div class="h-screen bg-cover bg-center bg-no-repeat bg-[url('./assets/in_progress.jpg')]">
         <div class="text-4xl text-center text-red-600 pt-20">This site is still in progress.</div>
@@ -13,7 +14,7 @@ function App() {
       {/* Code below is published on branch main. */}
       <Navbar />
       <About />
-
+      <Footer />
     </div>
   );
 }
---
+++ portfolio-site/src/components/Footer.js
@@ -0,0 +1,21 @@
+import {BrowserView, MobileView} from 'react-device-detect';
+import githubLogo from '../assets/githubLogo.png'
+
+export default function Footer() {
+    return(
+        <>
+            <BrowserView>
+                <footer className="bg-gradient-to-b from-primary to-secondary flex justify-center items-center gap-3 py-2 static bottom-0 w-full">
+                    <a href="https://github.com/laurance18" className="duration-700 hover:rotate-[360deg] hover:scale-110"><img src={githubLogo} alt="githubLogo" /></a> 
+                    <p className="text-gray-200 text-sm">Made by Laurance18</p>
+                </footer>
+            </BrowserView>
+            <MobileView>
+                <footer className="bg-gradient-to-b from-primary to-secondary flex justify-center items-center gap-3 py-2 static bottom-0 w-full">
+                    <a href="https://github.com/laurance18" className="duration-700 hover:rotate-[360deg] hover:scale-110"><img src={githubLogo} alt="githubLogo" /></a> 
+                    <p className="text-gray-200 text-sm">Made by Laurance18</p>
+                </footer>
+            </MobileView>
+        </>
+    )
+}
