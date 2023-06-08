abed7e95cd17b4cf63da023349ed9d6ee4c153a1
Deniz Işık <denizisik2004@gmail.com>
2023-06-08 14:16:14 +0300

    Update App.js

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <div class="h-screen bg-cover bg-center bg-no-repeat bg-[url('./assets/in_progress.jpg')]">
+    <div className="App bg-primary h-screen w-screen bg-contain">
+      {/* Code below is published on branch gh-pages. */}
+      {/* <div class="h-screen bg-cover bg-center bg-no-repeat bg-[url('./assets/in_progress.jpg')]">
         <div class="text-4xl text-center text-red-600 pt-20">This site is still in progress.</div>
-      </div>
+      </div> */}
+
+      {/* Code below is published on branch main. */}
+      <Navbar />
+
     </div>
   );
 }
