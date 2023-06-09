21e0707feace78594ef0a9e369452709baa1fa7e
Deniz Işık <denizisik2004@gmail.com>
2023-06-09 20:54:18 +0300

    Add projects related data

---
+++ portfolio-site/src/utils/projectsData.js
@@ -0,0 +1,21 @@
+import travel_photo from "../assets/travelsite_photo.png";
+
+export default {
+    "projects": [
+        {
+            id: 1,
+            name: "Travel Journal",
+            desc: `The "Deniz's Travel Journal" website is a web application that showcases my "travel" experiences. Users can see the photos of several sightseeing locations, as well as read a little description about each one. They can also look the location up on Google Maps. The site is built using ReactJS, and uses TailwindCSS framework for styling.`,
+            position: "text-left",
+            image: travel_photo,
+            link: "https://laurance18.github.io/travel_journal/"
+        },
+        {
+            id: 2,
+            name: "Site#2",
+            desc: "Test",
+            position: "text-right",
+            image: undefined,
+        },
+    ]
+}
