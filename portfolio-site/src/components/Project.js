437060750993618d1687763b62505f5eea56faa1
Deniz Işık <denizisik2004@gmail.com>
2023-06-10 11:33:18 +0300

    Update Project.js

--- portfolio-site/src/components/Project.js
+++ portfolio-site/src/components/Project.js
@@ -19,7 +19,7 @@ export default function Projects(props) {
                 <h4 className={`text-blue-400 italic font-bold text-xl md:px-5 pb-4 w-full`}>{props.name}</h4>
                 <span className="flex items-center flex-col md:flex-row gap-2 md:gap-5">
                     <span className="text-center w-full md:w-1/2 text-gray-300 pt-4 self-center">
-                        <p className="pb-5">{props.desc}</p>
+                        <p className="pb-7">{props.desc}</p>
                         <p>You can visit the website by clicking <a className="text-blue-800 hover:text-blue-600" href={props.link} target="_blank">this link.</a></p>
                     </span>
                     <img src={props.image} alt={props.name} className="w-full md:w-1/2" />
