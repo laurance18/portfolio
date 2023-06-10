d9ceffa12b59d768c991eef9c19537fc7246357b
Deniz Işık <denizisik2004@gmail.com>
2023-06-10 19:13:00 +0300

    Refactor navbar

--- portfolio-site/src/components/Navbar.js
+++ portfolio-site/src/components/Navbar.js
@@ -3,28 +3,13 @@ import {BrowserView, MobileView} from 'react-device-detect';
 export default function Navbar() {
 
     return (
-        <>
-            <BrowserView>
-                <nav className="bg-gradient-to-t from-primary to-secondary flex justify-between items-center py-4 px-6">
-                    <h2 className="text-white text-lg">Deniz Işık</h2>
-                    <div className="nav--links">
-                        <a href="#skills" className="text-white text-sm hover:text-gray-300 px-4">Skills</a>
-                        <a href="#projects" className="text-white text-sm hover:text-gray-300 px-4">Projects</a>
-                        <a href="#contacts" className="text-white text-sm hover:text-gray-300 px-4">Contact</a>
-                    </div>
-                </nav>
-            </BrowserView>
-            <MobileView>
-                <nav className="bg-gradient-to-t from-primary to-secondary flex justify-between xl:justify-between items-center py-4 px-6">
-                    <h2 className="text-white text-sm">Deniz Işık</h2>
-                    <div className="nav--links">
-                        <a href="#skills" className="text-white text-[10px] hover:text-gray-300 px-2">Skills</a>
-                        <a href="#projects" className="text-white text-[10px] hover:text-gray-300 px-2">Projects</a>
-                        <a href="#contact" className="text-white text-[10px] hover:text-gray-300 px-2">Contact</a>
-                    </div>
-                </nav>
-            </MobileView>
-            
-        </>
+        <nav className="bg-gradient-to-t from-primary to-secondary flex justify-between xl:justify-between items-center py-4 px-6">
+            <h2 className="text-white text-sm md:text-lg">Deniz Işık</h2>
+            <div className="nav--links">
+                <a href="#skills" className="text-white text-[10px] md:text-sm hover:text-gray-300 md:px-4 px-2">Skills</a>
+                <a href="#projects" className="text-white text-[10px] md:text-sm hover:text-gray-300 md:px-4 px-2">Projects</a>
+                <a href="#contact" className="text-white text-[10px] md:text-sm hover:text-gray-300 md:px-4 px-2">Contact</a>
+            </div>
+        </nav>
     )
 }
