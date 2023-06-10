08d345b923e4ae554e9ffa5338b98f65ce0e6025
Deniz Işık <denizisik2004@gmail.com>
2023-06-10 19:31:52 +0300

    Finish contacts

--- portfolio-site/src/components/Contact.js
+++ portfolio-site/src/components/Contact.js
@@ -1,6 +1,24 @@
 export default function Contact() {
     return (
-        <>
-        </>
+        <div id="contact" className="bg-primary py-10 flex flex-col md:flex-row justify-around items-center">
+            <div className="pb-5 md:"> 
+                <h2 className="text-white text-2xl md:text-4xl font-bold ">Get in touch!</h2>
+            </div>
+            <div className="">
+                {/* <span className="flex gap-4 items-center">
+                    <svg fill="#ffffff" width="32px" height="32px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M0 1694.235h1920V226H0v1468.235ZM112.941 376.664V338.94H1807.06v37.723L960 1111.233l-847.059-734.57ZM1807.06 526.198v950.513l-351.134-438.89-88.32 70.475 378.353 472.998H174.042l378.353-472.998-88.32-70.475-351.134 438.89V526.198L960 1260.768l847.059-734.57Z" fill-rule="evenodd"></path> </g></svg>
+                    <h3 className="text-white">denizisik2004@gmail.com</h3>
+                </span> */}
+                <span className="flex gap-4 items-center py-2">
+                    <svg fill="#ffffff" width="32px" height="32px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M0 1694.235h1920V226H0v1468.235ZM112.941 376.664V338.94H1807.06v37.723L960 1111.233l-847.059-734.57ZM1807.06 526.198v950.513l-351.134-438.89-88.32 70.475 378.353 472.998H174.042l378.353-472.998-88.32-70.475-351.134 438.89V526.198L960 1260.768l847.059-734.57Z" fill-rule="evenodd"></path> </g></svg>
+                    <h3 className="text-white">denizisik@std.iyte.edu.tr</h3>
+                </span>
+                <span className="flex gap-4 items-center">
+                    <svg fill="#ffffff" width="32px" height="32px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M478.234 600.75V1920H.036V600.75h478.198Zm720.853-2.438v77.737c69.807-45.056 150.308-71.249 272.38-71.249 397.577 0 448.521 308.666 448.521 577.562v737.602h-480.6v-700.836c0-117.867-42.173-140.215-120.15-140.215-74.134 0-120.151 23.55-120.151 140.215v700.836h-480.6V598.312h480.6ZM239.099 0c131.925 0 239.099 107.294 239.099 239.099s-107.174 239.099-239.1 239.099C107.295 478.198 0 370.904 0 239.098 0 107.295 107.294 0 239.099 0Z" fill-rule="evenodd"></path> </g></svg>
+                    <a href="https://www.linkedin.com/in/deniz-isik-me/" target="_blank" className="text-white pt-2 hover:text-gray-400">Deniz Işık</a>
+                </span>
+            </div>
+
+        </div>
     )
 }
