055d6a265372adabaf6bfc8739edc73944b23096
Deniz Işık <denizisik2004@gmail.com>
2023-06-16 01:03:11 +0300

    Update Project.js

--- portfolio-site/src/components/Project.js
+++ portfolio-site/src/components/Project.js
@@ -5,7 +5,7 @@ export default function Projects(props) {
         
         props.position !== "switched" || isMobile ? (
             <div className={`flex flex-col mx-8 my-5 p-4 rounded-lg bg-secondary`}>     
-                <h4 className={`text-blue-400 italic font-bold text-xl md:px-5 pb-4 w-full`}>{props.name}</h4>
+                <h4 className={`text-gray-100 italic font-bold text-xl md:px-5 pb-4 w-full`}>{props.name}</h4>
                 <span className="flex items-center flex-col md:flex-row gap-2 md:gap-5">
                     <img src={props.image} alt={props.name} className="w-full md:w-1/2" />
                     <span className="text-center w-full md:w-1/2 text-gray-300 pt-4 self-center">
@@ -16,7 +16,7 @@ export default function Projects(props) {
             </div>
         ) : (
             <div className={`flex flex-col mx-8 my-5 p-4 rounded-lg bg-secondary`}>     
-                <h4 className={`text-blue-400 italic font-bold text-xl md:px-5 pb-4 w-full`}>{props.name}</h4>
+                <h4 className={`text-gray-100 italic font-bold text-xl md:px-5 pb-4 w-full`}>{props.name}</h4>
                 <span className="flex items-center flex-col md:flex-row gap-2 md:gap-5">
                     <span className="text-center w-full md:w-1/2 text-gray-300 pt-4 self-center">
                         <p className="pb-7">{props.desc}</p>
